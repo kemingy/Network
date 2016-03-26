fa4bce38413321444a692eac16d5990ab2206b8b
Momingcoder <a398445075@gmail.com>
2016-03-26 09:41:30 +0800

    bingo

--- MAC-address/modify_mac_address.py
+++ MAC-address/modify_mac_address.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
+# @Author: Moming
+# 2016-03-26
 # modify MAC address
 # run this with administrative privileges if you want to change your MAC address
 # !!! This works under Chinese !!!
 
 # import os
-# import sys
-# import ctypes
+import sys
+import ctypes
 import re
 # import random
-# import platform
+import platform
 import _winreg as winreg
 import subprocess
 
@@ -37,9 +39,9 @@ if ctypes.windll.shell32.IsUserAnAdmin() == 0:
     sys.exit()
 
 # get the dict[link name : MAC address]
-link_name = re.findall(r'\r\n\xc1\xac\xbd\xd3\xc3\xfb:\s+(.+?)\r\n\xcd\xf8\xc2\xe7\xca\xca\xc5\xe4\xc6\xf7', mac_info)
+network_adapter = re.findall(r'\r\n\xcd\xf8\xc2\xe7\xca\xca\xc5\xe4\xc6\xf7:\s+(.+?)\r\n\xce\xef\xc0\xed\xb5\xd8\xd6\xb7', mac_info)
 mac_address = re.findall(r'\r\n\xce\xef\xc0\xed\xb5\xd8\xd6\xb7:\s+(.+?)\r\n\xb4\xab\xca\xe4\xc3\xfb\xb3\xc6', mac_info)
-name_mac = zip(link_name, mac_address)
+name_mac = zip(network_adapter, mac_address)
 name_mac_dict = dict(name_mac)
 
 def get_mac_address():
@@ -58,42 +60,43 @@ def get_device():
     get the device
     """
     print '==========================================================='
-    for name in link_name:
-        print name, ' | ',
+    index_list = range(len(network_adapter))
+    for index in index_list:
+        print index, ': ', network_adapter[index]
 
     print
-    input_name = raw_input('Please input the link name above you want to change: ')
-    while input_name not in link_name:
-        input_name = raw_input('Wrong input! Please input again: ')
+    index = input('Please input the device\'s index above you want to change: ')
+    while index not in index_list:
+        index = input('Wrong input! Please input again: ')
 
-    return input_name
+    return index, network_adapter[index]
 
-def restart_adapter(self, device):
+def restart_adapter(index):
     """
     Disables and then re-enables device interface
     """
     if platform.release() == 'XP':
-        description, adapter_name, address, current_address = find_interface(device)
+        # description, adapter_name, address, current_address = find_interface(device)
         cmd = "devcon hwids =net"
         try:
             result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
         except FileNotFoundError:
             raise
-        query = '('+description+'\r\n\s*.*:\r\n\s*)PCI\\\\(([A-Z]|[0-9]|_|&)*)'
+        query = '(' + target_device + '\r\n\s*.*:\r\n\s*)PCI\\\\(([A-Z]|[0-9]|_|&)*)'
         query = query.encode('ascii')
-        match = re.search(query,result)
-        cmd = 'devcon restart "PCI\\' + str(match.group(2).decode('ascii'))+ '"'
+        match = re.search(query, result)
+        cmd = 'devcon restart "PCI\\' + str(match.group(2).decode('ascii')) + '"'
         subprocess.check_output(cmd, stderr=subprocess.STDOUT)
 
     else:
-        cmd = "netsh interface set interface \"" + device + "\" disable"
+        cmd = "wmic path win32_networkadapter where index=" + str(index) + " call disable"
         subprocess.check_output(cmd)
-        cmd = "netsh interface set interface \"" + device + "\" enable"
+        cmd = "wmic path win32_networkadapter where index=" + str(index) + " call enable"
         subprocess.check_output(cmd)
 
-def set_mac_address():
+def set_mac_address(new_mac):
     """
-
+    set the device's MAC address
     """
     # Locate adapter's registry and update network address (mac)
     reg_hdl = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
@@ -103,10 +106,11 @@ def set_mac_address():
     # Find adapter key based on sub keys
     adapter_key = None
     adapter_path = None
+    target_index = -1
 
-    for x in range(info[0]):
-        subkey = winreg.EnumKey(key, x)
-        path = self.WIN_REGISTRY_PATH + "\\" + subkey
+    for index in range(info[0]):
+        subkey = winreg.EnumKey(key, index)
+        path = WIN_REGISTRY_PATH + "\\" + subkey
 
         if subkey == 'Properties':
             break
@@ -117,6 +121,7 @@ def set_mac_address():
             adapterDesc = winreg.QueryValueEx(new_key, "DriverDesc")
             if adapterDesc[0] == target_device:
                 adapter_path = path
+                target_index = index
                 break
             else:
                 winreg.CloseKey(new_key)
@@ -127,6 +132,7 @@ def set_mac_address():
                 raise err
 
     if adapter_path is None:
+        print 'Device not found.'
         winreg.CloseKey(key)
         winreg.CloseKey(reg_hdl)
         return
@@ -139,10 +145,19 @@ def set_mac_address():
     winreg.CloseKey(reg_hdl)
 
     # Adapter must be restarted in order for change to take affect
-    restart_adapter(adapter_name)
+    # print 'Now you should restart your netsh'
+    restart_adapter(target_index)
 
-target_device = get_device()
+index, target_device = get_device()
 print 'Your target device is: ' + target_device
 new_mac = get_mac_address()
-set_mac_address()
 print 'Your new mac is: ' + new_mac
+set_mac_address(new_mac)
+
+new_info = subprocess.check_output('GETMAC /v /FO list', stderr=subprocess.STDOUT)
+print new_info
+
+if raw_input('Want to reset? (yes / no)').lower() in ['yes', 'y']:
+    set_mac_address(name_mac_dict[target_device])
+    new_info = subprocess.check_output('GETMAC /v /FO list', stderr=subprocess.STDOUT)
+    print new_info
